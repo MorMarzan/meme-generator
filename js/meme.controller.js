ed5e4d807fe2459e497364ba58701f60813a8c0f
Mor Marzan <mormarzan@gmail.com>
2023-11-29 12:43:59 +0200

    Enable renderMeme() to read db meme and draw text

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -10,9 +10,26 @@ function onInit() {
 }
 
 function renderMeme() {
+    const canvasWidthCenter = gElCanvas.width / 2
+    const canvas20PerTop = gElCanvas.height * 0.2
+    const currMeme = getMeme()
+    
     const elImg = new Image()
     elImg.src = '/img/2.jpg'
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText(currMeme.lines[0].txt, canvasWidthCenter, canvas20PerTop)
     }
+
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '40px Impact'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,3 +1,28 @@
 'use strict'
 
-// console.log('service')
+var gImgs = [
+    { 
+        id: 1, 
+        url: 'img/2.jpg', 
+        keywords: ['sweet', 'dog'] 
+    },
+]
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I Love you',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+
+function getMeme(memeId) {
+    return gMeme
+}
