7e5c59bd46dd36f364610521c8c098a5c77cb8bb
Mor Marzan <mormarzan@gmail.com>
2023-11-29 12:05:38 +0200

    Implement basic renderMeme()

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,3 +1,18 @@
 'use strict'
 
-// console.log('controller')
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+}
+
+function renderMeme() {
+    const elImg = new Image()
+    elImg.src = '/img/2.jpg'
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    }
+}
