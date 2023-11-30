89004aed928de53ff2381e9ce1a20c127193c1c5
Mor Marzan <mormarzan@gmail.com>
2023-11-30 18:59:00 +0200

    Add responsive layout

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -9,6 +9,8 @@ function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
     addListeners()
+    resizeCanvas()
+
 }
 
 /* render and its helper funcs */
@@ -22,7 +24,8 @@ function renderMeme() {
     const elColorInput = document.querySelector(".editor .control-panel input[type=color]")
 
     const elImg = new Image()
-    elImg.src = getImgById(meme.selectedImgId).url
+    const imgId = getImgById(meme.selectedImgId)
+    elImg.src = (imgId) ? imgId.url : 'img/2.jpg'
 
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
@@ -181,12 +184,12 @@ function addListeners() {
     addTouchListeners()
     //Listen for resize ev
     // window.addEventListener('resize', () => {
-    //     resizeCanvas()
-    //     //Calc the center of the canvas
-    //     const center = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
-    //     //Create the circle in the center
-    //     createCircle(center)
-    //     renderCanvas()
+        // resizeCanvas()
+        //Calc the center of the canvas
+        // const center = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
+        //Create the circle in the center
+        // createCircle(center)
+        // renderMeme()
     // })
 }
 
@@ -243,11 +246,11 @@ function onUp() {
     // document.body.style.cursor = 'grab'
 }
 
-// function resizeCanvas() {
-//     const elContainer = document.querySelector('.canvas-container')
-//     gElCanvas.width = elContainer.offsetWidth
-//     gElCanvas.height = elContainer.offsetHeight
-// }
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
 
 function getEvPos(ev) {
 
@@ -269,3 +272,49 @@ function getEvPos(ev) {
     }
     return pos
 }
+
+/* share */
+function onUploadImg() {
+    // Gets the image from the canvas
+    const imgDataUrl = gElCanvas.toDataURL('image/jpeg') 
+
+    function onSuccess(uploadedImgUrl) {
+        // Handle some special characters
+        const url = encodeURIComponent(uploadedImgUrl)
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}&t=${url}`)
+    }
+    
+    // Send the image to the server
+    doUploadImg(imgDataUrl, onSuccess)
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+    // Pack the image for delivery
+    const formData = new FormData()
+    formData.append('img', imgDataUrl)
+
+    // Send a post req with the image to the server
+    const XHR = new XMLHttpRequest()
+    XHR.onreadystatechange = () => {
+        // If the request is not done, we have no business here yet, so return
+        if (XHR.readyState !== XMLHttpRequest.DONE) return
+        // if the response is not ok, show an error
+        if (XHR.status !== 200) return console.error('Error uploading image')
+        const { responseText: url } = XHR
+        // Same as
+        // const url = XHR.responseText
+
+        // If the response is ok, call the onSuccess callback function, 
+        // that will create the link to facebook using the url we got
+        console.log('Got back live url:', url)
+        onSuccess(url)
+    }
+    XHR.onerror = (req, ev) => {
+        console.error('Error connecting to server with request:', req, '\nGot response data:', ev)
+    }
+    XHR.open('POST', '//ca-upload.com/here/upload.php')
+    XHR.send(formData)
+}
+
+
+
