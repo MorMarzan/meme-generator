73b262c0fe47b959b0c828375b4c10f725e42cb2
Mor Marzan <mormarzan@gmail.com>
2023-12-02 17:50:49 +0200

    Add about dialog

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -329,5 +329,25 @@ function downloadImg(elLink) {
     elLink.href = imgContent
 }
 
+/* dialog */
+function onOpenDialog() {
+    const dialog = document.querySelector("dialog")
+    dialog.showModal()
+
+    dialog.addEventListener('click', function (event) {
+        var rect = dialog.getBoundingClientRect()
+        var isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+            rect.left <= event.clientX && event.clientX <= rect.left + rect.width)
+        if (!isInDialog) {
+            dialog.close()
+        }
+    })
+}
+
+function onCloseDialog() {
+    const dialog = document.querySelector("dialog")
+    dialog.close()
+}
+
 
 
